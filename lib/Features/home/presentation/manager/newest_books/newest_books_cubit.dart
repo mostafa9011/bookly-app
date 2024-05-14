f35f3eefdb6ffc8d37a445e1abda4cd014cabaa1
mostafa <mostafarasekh5@gamil.com>
2024-05-14 19:11:35 +0300

    create fetch newest books cubit

--- lib/Features/home/presentation/manager/newest_books/newest_books_cubit.dart
+++ lib/Features/home/presentation/manager/newest_books/newest_books_cubit.dart
@@ -1,24 +1,28 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../../data/models/book_model/book_model.dart';
-import '../../../data/repos/home_repo/home_repo.dart';
+
+import 'package:bookly/Features/home/domain/entities/book_entitiy.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
-  final HomeRepo homeRepo;
+  NewestBooksCubit(
+    this.fetchNewestBooksUseCase,
+  ) : super(NewestBooksInitial());
+  final FetchNewestBooksUseCase fetchNewestBooksUseCase;
 
   Future<void> fetchNewestBooks() async {
     emit(NewestBooksLoading());
-    var result = await homeRepo.fetchNewestBooks();
+    var result = await fetchNewestBooksUseCase.execute();
     result.fold((failure) {
       emit(
         NewestBooksFailure(failure.message),
       );
     }, (books) {
-      // emit(
-      //   NewestBooksSuccess(books),
-      // );
+      emit(
+        NewestBooksSuccess(books),
+      );
     });
   }
 }
